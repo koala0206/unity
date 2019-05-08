10c41ac3242188d8c1c7095a89ecf0ccd7687fcf
my koala <1151136628@qq.com>
2019-05-08 09:19:18 +0800

    1

---
+++ game/new unity project/Assets/button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+partial class ButtonClickTest :MonoBehaviour{
+	public Button m_btn;
+	public Text m_txt;
+	void Start(){
+		if (m_btn != null) {
+			m_btn.onClock.AddListener (() => {
+				UnityEngine.Debug.Log ("OnClickButton");
+				m_txt.texe = "已经点击";
+				m_txt.fontSize = 60;
+			});
+		}
+	}
+	void Update(){
+	}
+}
+ 
